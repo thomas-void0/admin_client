32eae5d63de72af9bbe851b6ae5ef9b52bbfd88a
lmxyjy <yjy15680489038@163.com>
2019-11-26 15:08:57 +0800

    echars 引入

--- src/component/charts/line.jsx
+++ src/component/charts/line.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ReactEcharts from "echarts-for-react";
+import {Card,Button} from 'antd'
 
-export default function Line() {
-    return (
-        <div>
-            line
-        </div>
-    )
+export default class Line extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            sales:[5,20,11,22,54,23],
+            stores:[6,10,25,20,64,13],
+        }
+    }
+    update = () => {
+        this.setState(state => ({
+          sales: state.sales.map(sale => sale + 1),
+          stores: state.stores.reduce((pre, store) => {
+            pre.push(store-1)
+            return pre
+          }, []),
+        }))
+    }
+    render() {
+        const title = (<Button type="primary" onClick={this.update}>更新</Button>)
+        const option = {
+            title:{
+                text:"销售情况"
+            },
+            legend: {
+                data:['销量', '库存']
+            },
+            tooltip: {},
+            xAxis: {
+                data: ["电动车","五菱宏光","川崎","长安","科鲁兹","科迈罗"]
+            },
+            yAxis: {},
+            series: [
+                {
+                    name:"销量",
+                    type: 'line',
+                    data:this.state.sales
+                },
+                {
+                    name:"库存",
+                    type: 'line',
+                    data:this.state.stores
+                },
+            ]
+        };
+        return (
+            <Card title={title}>
+                <ReactEcharts
+                    option={option}
+                    notMerge={true}
+                />
+            </Card>
+        )
+    }
 }
